4927a7bf39b414634cc43bc03031f03e2358efb1
Zhaorong Ma <mazhaorong@gmail.com>
2015-09-08 14:55:58 -0400

    add urls and views to run tasks

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,3 +1,21 @@
 from __future__ import absolute_import, unicode_literals
 
 # Create your views here.
+from django.http import HttpResponse
+import demoapp.tasks as tasks
+
+def home(request):
+    return HttpResponse('Try:<br>'
+                        'http://localhost:8000/add/1/2<br>'
+                        'or:<br>'
+                        'http://localhost:8000/mul/1/2'
+                        )
+
+def add(request, x, y):
+    tasks.add.delay(x, y)
+    return HttpResponse('task add is scheduled.')
+
+
+def mul(request, x, y):
+    tasks.mul.delay(x, y)
+    return HttpResponse('task mul is scheduled.')
--- proj/urls.py
+++ proj/urls.py
@@ -1,19 +1,12 @@
-from __future__ import absolute_import, unicode_literals
-
-from django.conf.urls import handler404, handler500, include, url  # noqa
+from django.urls import path  # noqa
+from demoapp import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'proj.views.home', name='home'),
-    # url(r'^proj/', include('proj.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    path('', views.home),
+    path('add/<int:x>/<int:y>/', views.add),
+    path('mul/<int:x>/<int:y>/', views.mul),
 ]
